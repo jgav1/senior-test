662326134670d58ab029752a9c9141caaa72553d
jgav <jorge.aravill@gmail.com>
2025-05-12 15:31:24 -0600

    Added routes for CR from CRUD
    Now we can get and post customers

--- api/public/customers/api.py
+++ api/public/customers/api.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter, Depends, status
 from api.utils.logger import logger_config
 from sqlmodel import Session
 from api.database import get_session
+from uuid import UUID
 from api.public.customers.models import(
     Customer,
-    CustomerCreate)
+    CustomerCreate, 
+    CustomerRead)
 from api.public.customers.crud import(
-    crud_get_customers,
-    create_customer_crud)
+    read_customer,
+    post_customer,
+    read_customers)
 
 
 
@@ -21,10 +24,15 @@ logger = logger_config(__name__)
 async def get_customers():
     return {"test endpoint": "reachable!"}
 
-@router.get("")
-async def get_customers():
-    return crud_get_customers()
+@router.get("", response_model=list[CustomerRead])
+async def get_all_customers(db: Session = Depends(get_session)):
+    return read_customers(db=db)
+
+
+@router.get("/{customer_id}", response_model=CustomerRead)
+async def get_customers(customer_id:UUID, db: Session = Depends(get_session)):
+    return read_customer(customer_id=customer_id, db=db)
 
 @router.post("")
 def create_a_customer(customer: CustomerCreate, db: Session = Depends(get_session)):
-    return create_customer_crud(customer=customer, db=db)
+    return post_customer(customer=customer, db=db)
