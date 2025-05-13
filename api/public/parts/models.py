8dff5336f7c789cc59ba0de00549620acc576271
jgav <jorge.aravill@gmail.com>
2025-05-13 12:27:43 -0600

    Updated Parts and SKUs models to include the relationship between both

--- api/public/parts/models.py
+++ api/public/parts/models.py
@@ -1,15 +1,14 @@
-from sqlmodel import Field, Relationship, SQLModel
-from typing import Optional, List
+from typing import TYPE_CHECKING, Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional
 from datetime import datetime, timezone
-from enum import Enum
 import uuid
-from pydantic import field_validator
-
-
-
+if TYPE_CHECKING:
+    from api.public.skus.models import Sku
+    from api.public.part_market_data.models import PartsMarketData
 
 class PartsBase(SQLModel):
-    quantity: int = Field(nullable=False)    
+    quantity: int = Field(nullable=False)
 
     model_config = {
         "json_schema_extra": {
@@ -23,18 +22,33 @@ class PartsBase(SQLModel):
 
 class Parts(PartsBase, table=True):
     id: uuid.UUID | None = Field(default_factory=uuid.uuid4, primary_key=True)
+    sku_id: uuid.UUID = Field(foreign_key="sku.id", nullable=False)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=True)
 
+    sku: Optional["Sku"] = Relationship(back_populates=None)  # one-way relationship
+    parts_market_data: Optional["PartsMarketData"] = Relationship(back_populates=None, cascade_delete=True)  # one-way relationship
+
 class PartsCreate(PartsBase):
-    pass
+    sku_id: uuid.UUID
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "sku_id": "123e4567-e89b-12d3-a456-426614174000",
+                    "quantity": "5",
+                }
+            ]
+        }
+    }
 
 class PartsRead(PartsBase):
     id: uuid.UUID
+    sku_id: uuid.UUID
     quantity: int
+
 class PartsUpdate(PartsBase):
     quantity: Optional[int] = None
-    
+    sku_id: Optional[uuid.UUID] = None
 
-
-   
+Parts.model_rebuild()
--- api/public/skus/models.py
+++ api/public/skus/models.py
@@ -1,9 +1,12 @@
-from sqlmodel import Field, Relationship, SQLModel
-from typing import Optional, List
+from typing import TYPE_CHECKING, Optional
+from sqlmodel import Field, SQLModel, Relationship
+from typing import Optional
 from datetime import datetime, timezone
 from enum import Enum
 import uuid
 from pydantic import field_validator
+if TYPE_CHECKING:
+    from api.public.parts.models import Parts
 
 class sizeEnum(str, Enum):
     xxs = "xxs"
@@ -17,9 +20,8 @@ class SkuBase(SQLModel):
     sku_value: str = Field(max_length=200, unique=True)
     size: sizeEnum = Field(default=sizeEnum.m, nullable=False)
     description: str = Field(max_length=200)
-    
 
-    @field_validator("sku_value","description", mode="before")
+    @field_validator("sku_value", "description", mode="before")
     @classmethod
     def lowercase_fields(cls, value: str) -> str:
         if isinstance(value, str):
@@ -32,7 +34,7 @@ class SkuBase(SQLModel):
                 {
                     "sku_value": "ABC-12345-S-BL",
                     "size": "xs",
-                    "description": "A small black t-shirt",
+                    "description": "Generic Automotive Part",
                 }
             ]
         }
@@ -42,21 +44,17 @@ class Sku(SkuBase, table=True):
     id: uuid.UUID | None = Field(default_factory=uuid.uuid4, primary_key=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=True)
+    parts: list["Parts"] = Relationship(back_populates=None, cascade_delete=True)  # one-way relationship
 
 class SkuCreate(SkuBase):
     pass
 
 class SkuRead(SkuBase):
     id: uuid.UUID
-    sku_value: str
-    size: sizeEnum
-    description: str
 
 class SkuUpdate(SkuBase):
     sku_value: Optional[str] = None
     size: Optional[sizeEnum] = None
     description: Optional[str] = None
-    
 
-
-   
+Sku.model_rebuild()
