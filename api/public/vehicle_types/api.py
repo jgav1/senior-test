18d81df190f58cf86a6f92ca86ebb689acad96f7
jgav <jorge.aravill@gmail.com>
2025-05-13 11:09:01 -0600

    refactor of vehicle types

--- api/public/vehicle_types/api.py
+++ api/public/vehicle_types/api.py
@@ -1,40 +1,15 @@
-from fastapi import APIRouter, Depends, status
-from api.utils.logger import logger_config
-from sqlmodel import Session
-from api.database import get_session
-from uuid import UUID
+# app/routes/vehicle_type_routes.py
+
+from fastapi import APIRouter
 from api.public.vehicle_types.models import *
-from api.public.vehicle_types.crud import *
+from api.public.vehicle_types.crud import crud_vehicle_types
+from api.utils.api_router import get_crud_router
 
 
-
-
-
-router = APIRouter()
-
-logger = logger_config(__name__)
-
-@router.get("/test")
-async def get_vehicles_type():
-    return {"test endpoint": "reachable!"}
-
-
-@router.get("", response_model=list[VehicleTypeRead])
-async def get_all_vehicles_types(db: Session = Depends(get_session)):
-    return read_vehicles_types(db=db)
-
-@router.get("/{vehicle_type_id}", response_model=VehicleTypeRead)
-async def get_vehicle_type(vehicle_type_id:UUID, db: Session = Depends(get_session)):
-    return read_vehicle_type(vehicle_type_id=vehicle_type_id, db=db)
-
-@router.post("", response_model=VehicleTypeRead)
-def create_vehicle_type(vehicletype: VehicleTypeCreate, db: Session = Depends(get_session)):
-    return post_vehicle_type(vehicletype=vehicletype, db=db)
-
-@router.patch("/{vehicle_type_id}", response_model=VehicleTypeRead)
-def update_vehicle_type(vehicle_type_id:UUID, vehicleType:VehicleTypeUpdate, db: Session = Depends(get_session)):
-    return patch_vehicle_type(vehicle_type_id=vehicle_type_id, vehicleType=vehicleType, db=db)
-
-@router.delete("/{vehicle_type_id}")
-def remove_vehicle_type(vehicle_type_id:UUID, db: Session = Depends(get_session)):
-    return delete_vehicle_type(vehicle_type_id=vehicle_type_id, db=db)
+router = get_crud_router(
+    model=VehicleType,
+    create_schema=VehicleTypeCreate,
+    update_schema=VehicleTypeUpdate,
+    read_schema=VehicleTypeRead,
+    crud=crud_vehicle_types,
+)
