2893e9a49e5bde1f2a9f2a71fed6cf7c429ed4a9
jgav <jorge.aravill@gmail.com>
2025-05-13 09:15:16 -0600

    refactoring customers CRUD

--- api/public/customers/api.py
+++ api/public/customers/api.py
@@ -1,39 +1,15 @@
-from fastapi import APIRouter, Depends, status
-from api.utils.logger import logger_config
-from sqlmodel import Session
-from api.database import get_session
-from uuid import UUID
+# app/routes/vehicle_type_routes.py
+
+from fastapi import APIRouter
 from api.public.customers.models import *
-from api.public.customers.crud import *
+from api.public.customers.crud import crud_customer_type
+from api.utils.api_router import get_crud_router
 
 
-
-
-
-router = APIRouter()
-
-logger = logger_config(__name__)
-
-@router.get("/test")
-async def get_customers():
-    return {"test endpoint": "reachable!"}
-
-@router.get("", response_model=list[CustomerRead])
-async def get_all_customers(db: Session = Depends(get_session)):
-    return read_customers(db=db)
-
-@router.get("/{customer_id}", response_model=CustomerRead)
-async def get_customers(customer_id:UUID, db: Session = Depends(get_session)):
-    return read_customer(customer_id=customer_id, db=db)
-
-@router.post("", response_model=CustomerRead)
-def create_customer(customer: CustomerCreate, db: Session = Depends(get_session)):
-    return post_customer(customer=customer, db=db)
-
-@router.patch("/{customer_id}", response_model=CustomerRead)
-def update_customer(customer_id:UUID, customer:CustomerUpdate, db: Session = Depends(get_session)):
-    return patch_customer(customer_id=customer_id, customer=customer, db=db)
-
-@router.delete("/{customer_id}")
-def remove_customer(customer_id:UUID, db: Session = Depends(get_session)):
-    return delete_customer(customer_id=customer_id, db=db)
+router = get_crud_router(
+    model=Customer,
+    create_schema=CustomerCreate,
+    update_schema=CustomerUpdate,
+    read_schema=CustomerRead,
+    crud=crud_customer_type,
+)
