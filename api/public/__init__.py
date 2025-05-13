a9ce359e67c5aba186cd59c915431428c1c54236
jgav <jorge.aravill@gmail.com>
2025-05-13 12:27:58 -0600

    added routes for parts and part market data

--- api/public/__init__.py
+++ api/public/__init__.py
@@ -3,6 +3,8 @@ from api.public.customers import api as customers
 from api.public.vehicles import api as vehicles
 from api.public.vehicle_types import api as vehicle_types
 from api.public.skus import api as sku
+from api.public.parts import api as parts
+from api.public.part_market_data import api as part_market_data
 #from api.auth import authent
 
 api = APIRouter()
@@ -34,5 +36,22 @@ api.include_router(
     tags=["skus"],
     #dependencies=[Depends(authent)],
 )
+
+api.include_router(
+    parts.router,
+    prefix="/parts",
+    tags=["parts"],
+    #dependencies=[Depends(authent)],
+)
+
+api.include_router(
+    part_market_data.router,
+    prefix="/part_market_data",
+    tags=["part_market_data"],
+    #dependencies=[Depends(authent)],
+)
+
+
+
 
 
